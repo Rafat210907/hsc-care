6c5ade9489cc8231aae9edc53efd047c329a61fc
rafat21 <0092powerstar@gmail.com>
2024-10-30 13:40:43 +0600

    Revert "popup image"
    This reverts commit b3d4ef155ff18ada3b61fced477a24e4bfbe0e8b.

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,31 +1,9 @@
-import { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import img1 from "../assets/tour/group.jpg";
-
+import img1 from "../assets/tour/group.jpg"
 const Slide = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentImage, setCurrentImage] = useState(null);
-
-  const openModal = (imgSrc) => {
-    setCurrentImage(imgSrc);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setCurrentImage(null);
-  };
-
-  useEffect(() => {
-    document.body.style.overflow = isModalOpen ? "hidden" : "auto";
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, [isModalOpen]);
-
-  const settings = {
+  var settings = {
     dots: true,
     infinite: true,
     speed: 800,
@@ -37,7 +15,7 @@ const Slide = () => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
+          slidesToScroll: 3,
           infinite: true,
           dots: true,
         },
@@ -46,7 +24,7 @@ const Slide = () => {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
+          slidesToScroll: 2,
           initialSlide: 2,
         },
       },
@@ -61,6 +39,7 @@ const Slide = () => {
   };
 
   return (
+<<<<<<< HEAD
 <<<<<<< HEAD
     <div className="carousel-container">
       <div className="carousel mt-20" data-aos="fade-up" data-aos-duration="1200">
@@ -93,8 +72,35 @@ const Slide = () => {
         <div className="box">
         <img src={img1} alt="Description 1" />
 >>>>>>> parent of aefd09a (gallery and slide responsive)
+=======
+    <div className="carousel mt-20" data-aos='fade-up' data-aos-duration="1200">
+       <h2  data-aos='fade-up' data-aos-duration="300" className="text-2xl sm:text-4xl text-center font-bold mb-1 sm:mb-8">
+        Gallery
+      </h2>
+      <Slider {...settings}>
+        <div className="box">
+        <img src={img1} alt="Description 1" />
+>>>>>>> parent of b3d4ef1 (popup image)
         </div>
-      )}
+        <div className="box">
+        <img src={img1} alt="Description 1" />
+        </div>
+        <div className="box">
+        <img src={img1} alt="Description 1" />
+        </div>
+        <div className="box">
+        <img src={img1} alt="Description 1" />
+        </div>
+        <div className="box">
+        <img src={img1} alt="Description 1" />
+        </div>
+        <div className="box">
+        <img src={img1} alt="Description 1" />
+        </div>
+        <div className="box">
+        <img src={img1} alt="Description 1" />
+        </div>
+      </Slider>
     </div>
   );
 };
