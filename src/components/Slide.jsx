b3d4ef155ff18ada3b61fced477a24e4bfbe0e8b
rafat21 <0092powerstar@gmail.com>
2024-10-30 12:56:35 +0600

    popup image

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import img1 from "../assets/tour/group.jpg"
+import img1 from "../assets/tour/group.jpg";
+
 const Slide = () => {
-  var settings = {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const openModal = (imgSrc) => {
+    setCurrentImage(imgSrc);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setCurrentImage(null);
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = isModalOpen ? "hidden" : "auto";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isModalOpen]);
+
+  const settings = {
     dots: true,
     infinite: true,
     speed: 800,
@@ -15,7 +37,7 @@ const Slide = () => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
@@ -24,7 +46,7 @@ const Slide = () => {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           initialSlide: 2,
         },
       },
@@ -39,33 +61,33 @@ const Slide = () => {
   };
 
   return (
-    <div className="carousel mt-20" data-aos='fade-up' data-aos-duration="1200">
-       <h2  data-aos='fade-up' data-aos-duration="300" className="text-2xl sm:text-4xl text-center font-bold mb-1 sm:mb-8">
-        Gallery
-      </h2>
-      <Slider {...settings}>
-        <div className="box">
-        <img src={img1} alt="Description 1" />
+    <div className="carousel-container">
+      <div className="carousel mt-20" data-aos="fade-up" data-aos-duration="1200">
+        <h2
+          data-aos="fade-up"
+          data-aos-duration="300"
+          className="text-2xl sm:text-4xl text-center font-bold mb-1 sm:mb-8"
+        >
+          Gallery
+        </h2>
+        <Slider {...settings}>
+          {[img1, img1, img1, img1, img1, img1, img1].map((image, index) => (
+            <div className="box" key={index} onClick={() => openModal(image)}>
+              <img src={image} alt={`Description ${index + 1}`} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close-button" onClick={closeModal}>&times;</span>
+            <img src={currentImage} alt="Enlarged view" />
+          </div>
         </div>
-        <div className="box">
-        <img src={img1} alt="Description 1" />
-        </div>
-        <div className="box">
-        <img src={img1} alt="Description 1" />
-        </div>
-        <div className="box">
-        <img src={img1} alt="Description 1" />
-        </div>
-        <div className="box">
-        <img src={img1} alt="Description 1" />
-        </div>
-        <div className="box">
-        <img src={img1} alt="Description 1" />
-        </div>
-        <div className="box">
-        <img src={img1} alt="Description 1" />
-        </div>
-      </Slider>
+      )}
     </div>
   );
 };
