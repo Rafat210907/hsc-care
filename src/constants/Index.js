c530757469f9779c74dc530ab6af28c8677f7b99
rafat21 <135577312+Rafat210907@users.noreply.github.com>
2024-11-07 09:54:12 +0600

    Updated teachers list

--- src/constants/Index.js
+++ src/constants/Index.js
@@ -9,8 +9,8 @@ export const teacherData = [
     id: 1,
     img: img,
     name: "Raju Deb" ,
-    qualificaions1: " BSS (Hons.) ",
-    qualificaions2: "MSS (Economics) ",
+    qualificaions1: " B S S (Hons.) ",
+    qualificaions2: "M S S (Economics) ",
     qualificaions3: "Special Masters (ICT)",
     profession: "Founder & CEO",
   },
@@ -26,6 +26,8 @@ export const teacherData = [
     img: img,
     name: "Koyes Akbor",
     profession: "Teacher",
+    qualifications1: "B B A (Hons.)",
+    qualifications2:"M B A (Accounting)",
     Facebook: "https://www.facebook.com/koyes.akbor.9?mibextid=ZbWKwL" 
   },
   {
@@ -33,8 +35,8 @@ export const teacherData = [
     img: img,
     name: "Proshanto Paul",
     profession: "Teacher",
-    qualificaions1: "Ba (Hons.)" ,
-    qualificaions2: " Ma (English)",
+    qualificaions1: "B A (Hons.)" ,
+    qualificaions2: " M A (English)",
     Facebook:" https://www.facebook.com/profile.php?id=100010094042256&mibextid=ZbWKwL"
   },
   {
@@ -42,7 +44,7 @@ export const teacherData = [
     img: img,
     name: "Shoaib Mahmud ",
     profession: "Teacher",
-    qualificaions4: "BSC (Hons.) Chemistry",
+    qualificaions4: "B S C (Hons.) Chemistry",
     institution: "Institute : SUST",
     Facebook: "https://www.facebook.com/profile.php?id=100008936828115&mibextid=ZbWKwL"
   },
@@ -51,8 +53,8 @@ export const teacherData = [
     img: img,
     name: "M A Mamun Maizvandari ",
     profession: "Teacher", 
-    qualificaions1: "BA (Hons.)" ,
-    qualificaions2: "MA (English) ", 
+    qualificaions1: "B A (Hons.)" ,
+    qualificaions2: "M A (English) ", 
     Facebook: "https://www.facebook.com/profile.php?id=100011713713782&mibextid=ZbWKwL"
   },
   {
@@ -69,7 +71,7 @@ export const teacherData = [
     img: img,
     name: "Noyan Deb ",
     profession: "Teacher", 
-    qualificaions1: "BSC (Hons.)" ,
-    qualificaions2: "MSC (Math) ", 
+    qualificaions1: "B S C (Hons.)" ,
+    qualificaions2: "M S C (Math) ", 
   },
 ];
