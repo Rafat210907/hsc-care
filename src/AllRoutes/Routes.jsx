060f77581bb0ee356142fadfd02ee0fffc9fb72c
rafat21 <0092powerstar@gmail.com>
2025-06-15 22:42:54 +0600

    send

--- src/AllRoutes/Routes.jsx
+++ src/AllRoutes/Routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import ContactUsPage from "../Pages/ContactUsPage";
 import ErrorPage from "../Pages/ErrorPage";
 import HomePage from "../Pages/HomePage";
-import Layout from "./AllRoutes/Layout.jsx"
+import Layout from "./Layout"
 import CarouselPage from "../Pages/CarouselPage";
 import AboutUsPage from "../Pages/AboutUsPage"
 import OurTeachers from "../Pages/OurTeachers";
