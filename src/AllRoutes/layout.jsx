5b34e58f3663ea8acefb17d4e4a89d119137b78d
rafat21 <0092powerstar@gmail.com>
2025-06-15 23:02:24 +0600

    v2 reupload

--- src/AllRoutes/layout.jsx
+++ src/AllRoutes/layout.jsx
@@ -1,16 +1,13 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
-import Footer from "../components/shared/Footer";
-import Header from "../components/shared/Header";
-
+import Header from "../components/Header";
 const Layout = () => {
   return (
     <>
       <div className="h-20 mb-10">
         <Header />
-      </div>
-      <Outlet />
+         <Outlet />
       <ScrollRestoration />
-      <Footer />
+    </div>
     </>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,23 @@
+import Header from "../components/Header";
 import Hero from "../components/Hero";
-import OurTeachers from "../components/OurTeachers";
-import AboutUs from "../components/AboutUs";
-import WhyUs from "../components/WhyUs";
-import Slide from "../components/Slide";
 
 const HomePage = () => {
-  return (
-    <>
-    <div className="font-sans">
-       <Hero />
-      <Slide />
-      <AboutUs />
-       <WhyUs />
-      <OurTeachers />
-    </div>
-    </>
+console.log("Rendering from HomePage.jsx");
+    return (
+    <main>
+      {/* Gradient Image */}
+      <img
+        className="absolute top-0 right-0 opacity-60 -z-1"
+        src="gradient.png"
+        alt="Gradient-img"
+      />
+
+      {/* Blur Effect */}
+      <div
+        className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#4D4DFF] -rotate-[30deg] -z-10"
+      ></div>
+      <Hero />
+    </main>
   );
 };
 
